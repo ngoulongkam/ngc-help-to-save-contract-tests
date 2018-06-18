dd5e5cacec06e38e3794452692fdaabd6d0a90b8
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2018-06-18 15:05:33 +0100

    NGC-3331 [FD] Stop using removed /enrol-user endpoint to set up test data

--- src/test/scala/uk/gov/hmrc/ngchelptosavecontract/mobilehelptosave/EnrolmentStatusSpec.scala
+++ src/test/scala/uk/gov/hmrc/ngchelptosavecontract/mobilehelptosave/EnrolmentStatusSpec.scala
@@ -20,10 +20,10 @@ import java.net.URL
 
 import org.scalatest.{AsyncWordSpec, Matchers}
 import play.api.Play
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsValue, Json}
 import play.api.test.{DefaultAwaitTimeout, FutureAwaits}
-import uk.gov.hmrc.domain.Generator
-import uk.gov.hmrc.http.{CoreGet, HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.domain.{Generator, Nino}
+import uk.gov.hmrc.http.{CoreGet, CorePost, HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.ngchelptosavecontract.scalatest.WSClientSpec
 import uk.gov.hmrc.ngchelptosavecontract.support.authloginapi.AuthLoginApiConnector
 import uk.gov.hmrc.ngchelptosavecontract.support.{ServicesConfig, TestWSHttp}
@@ -38,12 +38,12 @@ import scala.concurrent.{ExecutionContext, Future}
 class EnrolmentStatusSpec extends AsyncWordSpec with Matchers with FutureAwaits with DefaultAwaitTimeout with WSClientSpec {
   private val servicesConfig = new ServicesConfig
   private val authLoginApiConnector = new AuthLoginApiConnector(servicesConfig, wsClient)
-  private val http: CoreGet = new TestWSHttp(wsClient)
+  private val http: CoreGet with CorePost = new TestWSHttp(wsClient)
   private val generator = new Generator()
 
   private val helpToSaveBaseUrl: URL = new URL(servicesConfig.baseUrl("help-to-save"))
   private val enrolmentStatusUrl = new URL(helpToSaveBaseUrl, "/help-to-save/enrolment-status")
-  private val enrolUserUrl = new URL(helpToSaveBaseUrl, "/help-to-save/enrol-user")
+  private val createAccountUrl = new URL(helpToSaveBaseUrl, "/help-to-save/create-account")
 
   "/help-to-save/enrolment-status" should {
     """include "enrolled": false when user is not enrolled in Help to Save""" in {
@@ -57,9 +57,10 @@ class EnrolmentStatusSpec extends AsyncWordSpec with Matchers with FutureAwaits 
     }
 
     """include "enrolled": true when user is enrolled in Help to Save""" in {
-      authLoginApiConnector.governmentGatewayLogin(Some(generator.nextNino)).flatMap { implicit hc =>
+      val nino = generator.nextNino
+      authLoginApiConnector.governmentGatewayLogin(Some(nino)).flatMap { implicit hc =>
         for {
-          _ <- enrolUser()
+          _ <- createAccount(nino)
           enrolled <- enrolmentStatus()
         } yield {
           enrolled shouldBe true
@@ -68,8 +69,30 @@ class EnrolmentStatusSpec extends AsyncWordSpec with Matchers with FutureAwaits 
     }
   }
 
-  private def enrolUser()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] =
-    http.GET[HttpResponse](enrolUserUrl.toString).map { _ => () }
+  private def createAccount(nino: Nino)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+    val createAccountRequest = Json.parse(
+      s"""
+        |{
+        |  "userInfo": {
+        |    "forename": "TestForename",
+        |    "surname": "TestSurname",
+        |    "dateOfBirth": "19500101",
+        |    "nino": "${nino.value}",
+        |    "contactDetails": {
+        |      "address1": "1",
+        |      "address2": "2",
+        |      "postcode": "AA1 1AA",
+        |      "communicationPreference": "00"
+        |    },
+        |    "registrationChannel": "online"
+        |  },
+        |  "eligibilityReason": 6,
+        |  "source": "Digital"
+        |}
+      """.stripMargin
+    )
+    http.POST[JsValue, HttpResponse](createAccountUrl.toString, createAccountRequest).map { _ => () }
+  }
 
   private def enrolmentStatus()(implicit hc: HeaderCarrier, ec: ExecutionContext) =
     http.GET[JsValue](enrolmentStatusUrl.toString).map { json: JsValue =>
